43fecfa9bd468417d7ae964acc5a4ac9ac4c3a70
Nicole Frischknecht <nifrischknecht@gmail.com>
2020-01-08 10:14:35 +0100

    Erstellung neue Ordnerstruktur für alle Übungen

---
+++ HTML_CSS/02_Flexbox/ueb_01/ueb_01.md
@@ -0,0 +1,6 @@
+Übung 1: Froggy McFrogface
+============================
+
+
+Spiele das Flexbox Froggy Game einmal durch
+http://flexboxfroggy.com/
---
+++ HTML_CSS/04_SASS_SCSS/ueb_03/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDo-Liste</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Heebo:100&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+</body>
+</html>
