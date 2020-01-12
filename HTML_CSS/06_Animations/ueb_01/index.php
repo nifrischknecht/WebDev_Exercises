f0b80a07d07439bb4a6671aca8d8c8073cc18fed
Nicole Frischknecht <nifrischknecht@gmail.com>
2020-01-12 22:18:55 +0100

    Alle Übungen zu Animations und Transitions aktualisiert und gelöscht

---
+++ HTML_CSS/06_Animations/ueb_01/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Animation Wineglas</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Heebo:100&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="animation">
+        <div class="animation__text">
+            Loading a glas of redwine...
+        </div>
+        <div class="animation__fly">
+        </div>
+        <div class="animation__image">
+            <img src="images/glas-redwine.svg">
+        </div>
+    </div>
+
+</body>
+</html>
+
